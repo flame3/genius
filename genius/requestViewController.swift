b6fc1306ed8181dc98c692fcd2bacf311d764150
Nick <nick@Nicks-MacBook-Pro.local>
2019-05-16 06:51:25 -0400

    Printing out Json Object

--- genius/requestViewController.swift
+++ genius/requestViewController.swift
@@ -10,12 +10,33 @@ import UIKit
 
 class requestViewController: UIViewController {
 
+    let requstTV: UITableView = {
+        let tv = UITableView()
+        return tv
+    }()
+    
+    let urlSting = "https://rss.itunes.apple.com/api/v1/us/apple-music/coming-soon/all/10/explicit.json"
+    
+    
     // link for  media: https://rss.itunes.apple.com/en-us
+    //   https://rss.itunes.apple.com/api/v1/us/apple-music/coming-soon/all/10/explicit.json
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         
-        
+        view.backgroundColor = .yellow
+    }
+    
+    func downloadJsonWithUrl(){
+        let url = NSURL(string: urlSting)
+        URLSession.shared.dataTask(with: (url as URL?)!, completionHandler: { (data, response, error) in
+            if let jo = try? JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? NSDictionary{
+                print(jo)
+
+            }
+            
+        }).resume()
     }
     
 

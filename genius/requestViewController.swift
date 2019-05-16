8673e9c81deedfba70628a8764a0c7f6113c7cf6
Nick <nick@Nicks-MacBook-Pro.local>
2019-05-15 21:58:41 -0400

    Initial Updload

---
+++ genius/requestViewController.swift
@@ -0,0 +1,23 @@
+//
+//  requestViewController.swift
+//  genius
+//
+//  Created by Nick on 5/15/19.
+//  Copyright © 2019 nicksdev. All rights reserved.
+//
+
+import UIKit
+
+class requestViewController: UIViewController {
+
+    // link for  media: https://rss.itunes.apple.com/en-us
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+
+
+}
